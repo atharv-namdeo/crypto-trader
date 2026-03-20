a6d933a989df2cef6162ded3d565b33076ea1539
Atharv Namdeo <namdeoatharv0@gmail.com>
2026-03-21 03:55:53 +0530

    Add Vercel deployment support

--- dashboard/src/firebase.js
+++ dashboard/src/firebase.js
@@ -1,16 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// User needs to replace this with their actual Firebase config
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBtkOrp9Iizskp9-7KmFS8SsPpEMFWkfSs",
+    authDomain: "quant-ac5b9.firebaseapp.com",
+    projectId: "quant-ac5b9",
+    storageBucket: "quant-ac5b9.firebasestorage.app",
+    messagingSenderId: "229378767288",
+    appId: "1:229378767288:web:7a677c60c73ff2eb7e774d",
+    measurementId: "G-4NNBXEP9L9"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
